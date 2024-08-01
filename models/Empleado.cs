b7df8813efc78cdb0dcc341efea6b6e737e1c5bf
val <valeria@gmail.com>
2024-08-01 07:35:50 -0500

    feat: se empieza a heredar empleado de persona

--- models/Empleado.cs
+++ models/Empleado.cs
@@ -5,23 +5,16 @@ using System.Threading.Tasks;
 
 namespace Gestion_Empleados.models
 {
-    public class Empleado
+    public class Empleado:Persona
     {
-        public Guid Id { get; set; }
-        public string? Nombre { get; set; }
-        public string? Apellido { get; set; }
-        public string? NumeroDeIdentificacion { get; set; }
-        public byte Edad { get; set; }
+        // atributos de empleado
         public string? Posicion { get; set; }
         public double Salario { get; set; }
 
+
         public Empleado(string nombre, string apellido, string numeroDeDocumento, byte edad, string posicion, double salario)
         {
-            Id = Guid.NewGuid();
-            Nombre = nombre;
-            Apellido = apellido;
-            NumeroDeIdentificacion = numeroDeDocumento;
-            Edad = edad;
+            
             Posicion = posicion;
             Salario = salario;
         }
--- models/Persona.cs
+++ models/Persona.cs
@@ -7,6 +7,10 @@ namespace Gestion_Empleados.models
 {
     public class Persona
     {
-        
+        public Guid Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? NumeroDeIdentificacion { get; set; }
+        public byte Edad { get; set; }
     }
 }
